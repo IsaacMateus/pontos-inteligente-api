a0d8e0b1dd701d274e482a212e97433d56018f80
Isaac Abreu <isaacmateusaraujo@gmail.com>
2021-07-01 18:57:45 -0300

    primeiro commite

---
+++ src/main/java/com/isaac/pontosinteligente/PontosInteligenteApplication.java
@@ -0,0 +1,13 @@
+package com.isaac.pontosinteligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontosInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontosInteligenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isaac/pontosinteligente/PontosInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaac.pontosinteligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontosInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
